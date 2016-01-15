f1b43e87f4529723d47b655b0aa2e86d45b97681
limejuggler <limejuggler@users.noreply.github.com>
2016-01-15 12:12:11 +0100

    Added example code to create a twitterListener

---
+++ TwitterListener/TwitterListener/src/main/java/com/nordea/twitterlistener/Listener.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nordea.twitterlistener;
+
+
+import java.util.List;
+import javax.management.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author Emil
+ */
+public class Listener
+{
+    public void listenForTag()
+    {
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            Query query = new Query();
+            QueryResult result;
+            do {
+                result = twitter.search(query);
+                List<Status> tweets = result.getTweets();
+                for (Status tweet : tweets) {
+                    System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+                }
+            } while ((query = result.nextQuery()) != null);
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+}
