c9f6207165d133cc17889249113688ba10e92d0a
limejuggler <limejuggler@users.noreply.github.com>
2016-01-15 11:18:07 +0100

    Number generator for Fibonacci and prime numbers

---
+++ NumberGenerator/NumberGenerator/src/main/java/com/nordea/numbergenerator/Generator.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nordea.numbergenerator;
+
+/**
+ *
+ * @author Emil
+ */
+public class Generator 
+{
+    private int fibNext = 1, fibCur = 0, fibPrev = 0;
+    private int nextPrime = 0;
+    public String getNextFibonacciNumber()
+    {
+        if(0 == fibCur)
+        {
+            fibCur = fibNext;
+            return "0";
+        }
+        if(1 == fibNext)
+        {
+            fibCur = fibNext;
+            fibNext = fibCur + fibPrev;
+            fibPrev = fibCur;
+            return fibCur + "";
+        }
+        
+        fibNext = fibCur + fibPrev;
+        fibPrev = fibCur;
+        fibCur = fibNext;
+        
+        return fibCur + "";
+    }
+    
+    public String getPrimeNumbers()
+    {
+        boolean potentialPrimeNumber = true;
+        while(true)
+        {
+            nextPrime++;
+            if(1 == nextPrime || 2 == nextPrime || 3 == nextPrime)
+            {
+                break;
+            }
+            if(nextPrime % 2 == 0)
+            {
+                nextPrime++;
+            }
+            
+            for(int i = 3; i < Math.floor(nextPrime / 2) && potentialPrimeNumber; i++)
+            {
+                if(0 == nextPrime % i)
+                    potentialPrimeNumber = false;
+            }
+            if(potentialPrimeNumber)
+                break;
+            
+            potentialPrimeNumber = true;
+        }
+        
+        return nextPrime + "";
+    }
+}
